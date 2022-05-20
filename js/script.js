b54dcb57ad4053f460784a327f88f8a5f5a08205
unknown <pecordoba_ds@hotmail.com>
2022-05-20 15:24:31 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -5,17 +5,21 @@ function calcular()
   console.log(cantidad)
   console.log(categoria)
 
-
+  if(cantidad>0){
    switch(categoria)
        {
              case 'Estudiante':
-                 document.getElementById('result').value=("Total a pagar: $ "+((200*0.5)*cantidad))
+                 document.getElementById('result').value=("Total a pagar: $ "+((200*0.2)*cantidad))
                  break;
              case 'Trainee':
                  document.getElementById('result').value=("Total a pagar: $ "+((200*0.5)*cantidad))
                  break;
              case 'Junior':
-                 document.getElementById('result').value=("Total a pagar: $ "+((200*0.5)*cantidad))
+                 document.getElementById('result').value=("Total a pagar: $ "+((200*0.85)*cantidad))
                  break;
        }
+    }else{
+        alert('Ingrese cantidad de entradas')
+    }
+
 }
